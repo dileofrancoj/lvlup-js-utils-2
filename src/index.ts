5ea6645e9ac45dd5f18874c82405b8857b15ec11
Franco Di Leo <dileo.francoj@gmail.com>
2024-06-03 19:46:34 -0300

    chore: initialize library

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export const fn = (someParameter:string): void => {
+    console.log('someParameter', someParameter)
+}
---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+// yarn add -D @types/node
+// yarn add -D vite-plugin-dts
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
+
+export default defineConfig({
+    build: {
+        lib: {
+            entry: resolve(__dirname, 'src/index.ts'),
+            name: 'lvlup-js-utils'
+        },
+        sourcemap: true,
+        target: 'es6',
+        minify: false
+    },
+    plugins: [dts({outDir: 'dist'})]
+})
